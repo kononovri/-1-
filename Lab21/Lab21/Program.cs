fc634a285f6657cc88ee68582c0e1e63cfcbc3a5
Роман Кононов <kononovri@gmail.com>
2022-01-15 17:26:12 +0300

    1.0

---
+++ Lab21/Lab21/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab21
+{
+    class Program
+    {
+
+
+        static int w;
+        static int h;
+        static int[,] garden;
+        static object locker = new object();
+
+        static void Main()
+        {
+            Console.WriteLine("Введите размеры сада:");
+            Console.WriteLine("По вертикали:");
+            h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("По горизонтали:");
+            w = Convert.ToInt32(Console.ReadLine());
+            garden = new int[h, w];
+            ThreadStart threadStart = new ThreadStart(garden1);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+            garden2();
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    Console.Write(garden[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        private static void garden1()
+        {
+            lock (locker)
+            {
+                for (int i = 0; i < h; i++)
+                {
+                    for (int j = 0; j < w; j++)
+                    {
+                        if (garden[i, j] == 0)
+                            garden[i, j] = 1;
+                        Thread.Sleep(1);
+                    }
+                }
+            }
+        }
+        private static void garden2()
+        {
+            for (int i = w - 1; i >= 0; i--)
+            {
+                for (int j = h - 1; j >= 0; j--)
+                {
+                    if (garden[j, i] == 0)
+                        garden[j, i] = 2;
+                    Thread.Sleep(1);
+                }
+            }
+        }
+    }
+}
