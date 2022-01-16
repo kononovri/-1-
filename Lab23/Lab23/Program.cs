7eb4938be704832450a4efe32518e2c58b2b016d
Роман Кононов <kononovri@gmail.com>
2022-01-16 14:49:48 +0300

    1.0

--- Lab23/Lab23/Program.cs
+++ Lab23/Lab23/Program.cs
@@ -2,14 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lab23
 {
     class Program
     {
-        static void Main(string[] args)
+        static public void Main(string[] args)
         {
+            Console.WriteLine("Main начал работу");
+            Console.WriteLine("Введите натуральное число для расчета факториала этого числа");
+            int number = Convert.ToInt32(Console.ReadLine());
+            _ = FactorialAsync(number);
+            Console.WriteLine("Main закончил работу");
+            Console.ReadKey();
+        }
+        static public void Factorial(int number)
+        {
+            Console.WriteLine("Factorial начал работу");
+
+            long result = 1;
+            for (long i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"Факториал числа {number}!={result}");
+            Console.WriteLine("Factorial закончил работу");
+            Console.ReadKey();
+        }
+        static public async Task FactorialAsync(int number)
+        {
+            Console.WriteLine("FactorialAsync начал работу");
+            await Task.Run(() => Factorial(number));
+            Console.WriteLine("FactorialAsync закончил работу");
+            Console.ReadKey();
         }
     }
 }
