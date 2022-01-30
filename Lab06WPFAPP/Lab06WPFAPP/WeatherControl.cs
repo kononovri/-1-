5c8c846485e4e60461099fd14445300500e3c047
Роман Кононов <kononovri@gmail.com>
2022-01-30 13:22:45 +0300

    1.0

---
+++ Lab06WPFAPP/Lab06WPFAPP/WeatherControl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab06WPFAPP
+{
+    class WeatherControl : DependencyObject
+    {
+        public static readonly DependencyProperty TemperatureProperty = DependencyProperty.Register(
+      nameof(Temperature),
+      typeof(int),
+      typeof(WeatherControl),
+      new FrameworkPropertyMetadata(
+          0,
+          FrameworkPropertyMetadataOptions.AffectsMeasure |
+          FrameworkPropertyMetadataOptions.AffectsRender,
+          null,
+          new CoerceValueCallback(CoerceTemperature)),
+      new ValidateValueCallback(ValidateTemperature));
+
+        private static bool ValidateTemperature(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+                return true;
+            else
+                return false;
+        }
+
+        private static object CoerceTemperature(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+                return v;
+            else
+                return 0;
+        }
+
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty, value);
+        }
+
+        public string windDirection;
+        public int windSpeed;
+
+        public enum Precipitation
+        {
+            солнечно = 0,
+            облачно = 1,
+            дождь = 2,
+            снег = 3
+        }
+    }
+}
