24782bcd377228c92f411190753b6fc4965953a2
Роман Кононов <kononovri@gmail.com>
2022-03-12 16:47:01 +0300

    1

---
+++ Lab19WPFAPP/Lab19WPFAPP/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19WPFAPP.Models
+{
+    static class Ariph
+    {
+        public static double Add(double r) => 2 * Math.PI * r;
+    }
+}
---
+++ Lab19WPFAPP/Lab19WPFAPP/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Lab19WPFAPP.Models;
+
+namespace Lab19WPFAPP.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+        private double length;
+        public double Length
+        {
+            get => length;
+            set
+            {
+                length = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Length = Ariph.Add(Radius);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Radius !=0)
+                return true;
+            else
+                return
+                    false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+        }
+}
